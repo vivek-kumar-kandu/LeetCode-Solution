bf9b9aa60ea148002dba2c9aee884a121c0d2113
Vivek Kumar Kandu <vivekk11951@gmail.com>
2026-08-04 18:38:42 +0530

    Time: 115 ms (11.53%), Space: 43 MB (9.44%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -4,8 +4,8 @@ public:
         set<vector<int>> ans;
         sort(nums.begin(), nums.end());
         for (int i = 0; i < nums.size(); i++) {
-               if (i > 0 && nums[i] == nums[i - 1])
-               continue;
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
             int left = i + 1;
             int right = nums.size() - 1;
             while (left < right) {
