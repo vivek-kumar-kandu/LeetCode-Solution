15c498cf0505b7bea93be830e8b7c1bc4df9d302
Vivek Kumar Kandu <vivekk11951@gmail.com>
2026-09-18 11:22:21 +0530

    Time: 20 ms (5.5%), Space: 263.2 MB (51.18%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -3,48 +3,49 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n=nums.size();
         k=k%n;
-        int left=0;
-        int right=n-1;
-        while(left<right){
-            int temp = nums[left];
-            nums[left]=nums[right];
-            nums[right]=temp;
-            left++;
-            right--;
-        }
-        left=0;
-        int mid=k-1;
-        while(left<mid){
-            int temp = nums[left];
-            nums[left]=nums[mid];
-            nums[mid]=temp;
-            left++;
-            mid--;
-        }
-        left=k;
-        right=n-1;
-        while(left<right){
-            int temp = nums[left];
-            nums[left]=nums[right];
-            nums[right]=temp;
-            left++;
-            right--;
-        }
-        // int first_part=n-k;
-        // int second_part=k;
+        // 1st 
         // int left=0;
         // int right=n-1;
         // while(left<right){
-            // swap(nums[left],nums[right]);
-            // left++;
-            // right--;
+        //     int temp = nums[left];
+        //     nums[left]=nums[right];
+        //     nums[right]=temp;
+        //     left++;
+        //     right--;
+        // }
+        // left=0;
+        // int mid=k-1;
+        // while(left<mid){
+        //     int temp = nums[left];
+        //     nums[left]=nums[mid];
+        //     nums[mid]=temp;
+        //     left++;
+        //     mid--;
+        // }
+        // left=k;
+        // right=n-1;
+        // while(left<right){
+        //     int temp = nums[left];
+        //     nums[left]=nums[right];
+        //     nums[right]=temp;
+        //     left++;
+        //     right--;
+        // }
+        
+
+        // int left=0;
+        // int right=n-1;
+        // while(left<right){
+        //     swap(nums[left],nums[right]);
+        //     left++;
+        //     right--;
         // }
         // left=0;
         // right=k-1;
         // while(left<right){
-            // swap(nums[left],nums[right]);
-            // left++;
-            // right--;
+        //     swap(nums[left],nums[right]);
+        //     left++;
+        //     right--;
         // }
         // left=k,right=n-1;
         // while(left<right){
@@ -52,11 +53,16 @@ public:
         //     left++;
         //     right--;
         // }
-        // vector<int>ans(n);
-        // for(int i=0;i<n;i++){
-        //     ans[(i+k)%n]=nums[i];
-        // }
-        // nums=ans;
+
+
+
+        vector<int>ans(n);
+        for(int i=0;i<n;i++){
+            ans[(i+k)%n]=nums[i];
+        }
+        nums=ans;
+
+        
         // reverse(nums.begin(),nums.end());
         // reverse(nums.begin(),nums.begin()+k);
         // reverse(nums.begin()+k,nums.end());
