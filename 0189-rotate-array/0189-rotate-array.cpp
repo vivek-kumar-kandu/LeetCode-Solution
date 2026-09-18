013a0df787ba03fffe028ddea04d4cf59595fafc
Vivek Kumar Kandu <vivekk11951@gmail.com>
2026-09-18 10:54:33 +0530

    Time: 0 ms (100%), Space: 263.5 MB (19.99%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -3,20 +3,33 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n=nums.size();
         k=k%n;
-        // int first_part=n-k;
-        // int second_part=k;
-        // int left=0;
-        // int right=n-1;
-        // while(left<right){
-        //     swap(nums[left],nums[right]);
-        //     left++;
-        //     right--;
-        // }
-        vector<int>ans(n);
-        for(int i=0;i<n;i++){
-            ans[(i+k)%n]=nums[i];
+        int first_part=n-k;
+        int second_part=k;
+        int left=0;
+        int right=n-1;
+        while(left<right){
+            swap(nums[left],nums[right]);
+            left++;
+            right--;
         }
-        nums=ans;
+        left=0;
+        right=k-1;
+        while(left<right){
+            swap(nums[left],nums[right]);
+            left++;
+            right--;
+        }
+        left=k,right=n-1;
+        while(left<right){
+            swap(nums[left],nums[right]);
+            left++;
+            right--;
+        }
+        // vector<int>ans(n);
+        // for(int i=0;i<n;i++){
+        //     ans[(i+k)%n]=nums[i];
+        // }
+        // nums=ans;
         // reverse(nums.begin(),nums.end());
         // reverse(nums.begin(),nums.begin()+k);
         // reverse(nums.begin()+k,nums.end());
